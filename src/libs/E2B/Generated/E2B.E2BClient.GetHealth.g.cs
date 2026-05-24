ed25dbe8a9d6b4111550a2806239b02e7476ff71
Konstantin S. <havendv@gmail.com>
2026-05-24 10:31:22 +0400

    feat: Updated OpenAPI spec (#37)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/E2B/Generated/E2B.E2BClient.GetHealth.g.cs
+++ src/libs/E2B/Generated/E2B.E2BClient.GetHealth.g.cs
@@ -333,18 +333,17 @@ namespace E2B
                                     __exception_401 = __ex;
                                 }
 
-                                throw new global::E2B.ApiException<global::E2B.Error>(
+
+                                throw global::E2B.ApiException<global::E2B.Error>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_401 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_401,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_401,
-                                    ResponseObject = __value_401,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_401,
+                                    responseObject: __value_401,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -371,17 +370,15 @@ namespace E2B
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::E2B.ApiException(
+                                    throw global::E2B.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -409,17 +406,15 @@ namespace E2B
                                     {
                                     }
 
-                                    throw new global::E2B.ApiException(
+                                    throw global::E2B.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
