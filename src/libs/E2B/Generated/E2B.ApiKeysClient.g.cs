f72b711db26453db1c1d7b46756f8a1e0b8034d7
Konstantin S. <havendv@gmail.com>
2026-09-13 21:56:28 +0400

    feat: Updated OpenAPI spec (#65)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/E2B/Generated/E2B.ApiKeysClient.g.cs
+++ src/libs/E2B/Generated/E2B.ApiKeysClient.g.cs
@@ -34,7 +34,7 @@ namespace E2B
         /// <inheritdoc/>
         public global::E2B.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::E2B.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::E2B.ApiKeysSourceGenerationContext.Default);
 
         /// <summary>
         ///
